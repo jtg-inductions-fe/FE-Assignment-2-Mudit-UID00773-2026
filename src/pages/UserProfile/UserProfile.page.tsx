c69dc072a2c96d56bf456998005bbffeb4b55558
muditsingh-sudo <mudit.singh@joshtechnologygroup.com>
2026-08-26 12:55:09 +0530

    [MK_A2_04]: Intitial Commit

--- src/pages/UserProfile/UserProfile.page.tsx
+++ src/pages/UserProfile/UserProfile.page.tsx
@@ -1,5 +1,31 @@
-import { Box } from '@mui/material';
+import { Box, styled, Typography } from '@mui/material';
 
-const UserProfile = () => <Box>User Profile</Box>;
+import image from '@assets/images/Logo.png';
+
+export const UserProfileContainer = styled(Box)(() => ({
+    minHeight: '100vh',
+    background: 'beige',
+    paddingTop: '12vh',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+}));
+
+export const UserProfileHeading = styled(Typography)(({}) => ({
+    fontSize: '2px',
+})) as typeof Typography;
+
+const UserProfile = () => (
+    <UserProfileContainer>
+        <Box>
+            <img src={image} alt="" />
+        </Box>
+        <Box>
+            <UserProfileHeading variant="h1">
+                Mudit Kumar Singh
+            </UserProfileHeading>
+        </Box>
+    </UserProfileContainer>
+);
 
 export default UserProfile;
