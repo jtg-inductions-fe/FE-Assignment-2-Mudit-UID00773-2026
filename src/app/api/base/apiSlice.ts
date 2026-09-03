b2b25448406169f98fd0cc3bbb1f0586eacd06ed
muditsingh-sudo <mudit.singh@joshtechnologygroup.com>
2026-09-03 18:54:53 +0530

    [MK_A2_05]: Login page, Myprofile page, auth logic

--- src/app/api/base/apiSlice.ts
+++ src/app/api/base/apiSlice.ts
@@ -1,8 +1,16 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { getTokenFromLocalStorage } from '@utils';
 
 export const apiSlice = createApi({
     baseQuery: fetchBaseQuery({
         baseUrl: 'https://api.github.com',
+        prepareHeaders: (headers) => {
+            const token = getTokenFromLocalStorage();
+
+            if (token) {
+                headers.set('authorization', `Bearer ${token}`);
+            }
+        },
     }),
     tagTypes: ['User', 'Profile'],
     endpoints: () => ({}),
--- src/pages/MyProfile/MyProfile.page.tsx
+++ src/pages/MyProfile/MyProfile.page.tsx
@@ -1,5 +1,18 @@
-import { Box } from '@mui/material';
+import { useLoginQuery } from '@app/api/auth/authApiSlice';
+import { UserDetailsCard } from '@components';
+import { getTokenFromLocalStorage } from '@utils';
 
-const MyProfile = () => <Box>MyProfile</Box>;
+import { MyProfileContainer } from './MyProfile.styles';
+
+const MyProfile = () => {
+    const token = getTokenFromLocalStorage();
+    const { data: user } = useLoginQuery(token);
+
+    return (
+        <MyProfileContainer minHeight="100vh" paddingTop="12vh">
+            {user && <UserDetailsCard {...user} />}
+        </MyProfileContainer>
+    );
+};
 
 export default MyProfile;
