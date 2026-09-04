e37fe242e05953f0de8ea4583810ca1edcc2c439
muditsingh-sudo <mudit.singh@joshtechnologygroup.com>
2026-09-04 13:12:58 +0530

    [MK_A2_05]: rabbit fixes 2

--- src/pages/MyProfile/MyProfile.page.tsx
+++ src/pages/MyProfile/MyProfile.page.tsx
@@ -8,7 +8,7 @@ import { MyProfileContainer } from './MyProfile.styles';
 
 const MyProfile = () => {
     const token = getTokenFromLocalStorage();
-    const { data: user, isLoading } = useLoginQuery(token);
+    const { data: user, isLoading } = useLoginQuery(token, { skip: !token });
 
     return (
         <MyProfileContainer minHeight="100vh" paddingTop="12vh">
